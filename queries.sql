4c6be74844e602141fb91f6da60be1ec2e40afff
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-06-07 17:59:02 +0300

    Add updated queries.sql

--- queries.sql
+++ queries.sql
@@ -15,8 +15,45 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 -- Find all animals with a weight between 10.4kg and 17.3kg (inclusive).
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+-- Update the "species" column for all rows in the "animals" table to "unspecified":
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
 
--- add-query-update branch
+BEGIN;
+-- Update the "species" column to "digimon" for animals with names ending in "mon":
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+-- Update the "species" column to "pokemon" for animals that don't have a species already 
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+-- Commit the transaction to persist the changes:
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+-- Delete all records in the "animals" table:
+DELETE FROM animals;
+-- Roll back the transaction to undo the deletions
+ROLLBACK;
+SELECT * FROM animals;
+
+
+BEGIN;
+-- Delete all animals born after Jan 1st, 2022:
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+-- Create a savepoint for the transaction
+SAVEPOINT my_savepoint;
+-- Update all animals' weight to be their weight multiplied by -1
+UPDATE animals SET weight_kg = weight_kg * -1;
+-- Roll back to the savepoint
+ROLLBACK TO SAVEPOINT my_savepoint;
+-- Update all animals' weights that are negative to be their weight multiplied by -1
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+-- Commit the transaction to persist the changes
+COMMIT;
+
 -- How many animals are there?
 SELECT COUNT(*) FROM animals;
 -- How many animals have never tried to escape?
