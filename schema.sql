651196d54e534cbc9d422332d3ac98e0534ba10e
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-06-10 15:01:17 +0300

    Add updated schemal.sql

--- schema.sql
+++ schema.sql
@@ -10,3 +10,20 @@ CREATE TABLE animals (
 -- add-query-update branch
 
 ALTER TABLE animals ADD COLUMN species varchar(100);
+
+-- query-multiple-tables branch
+
+-- Create the "owners" table with the specified columns
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(255),
+  age INTEGER
+);
+-- Create the "species" table with the specified columns
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+-- Modify the "animals" table to set the "id" column as auto-incremented primary key
+ALTER TABLE animals
+ALTER COLUMN id SET DATA TYPE SERIAL PRIMARY KEY;
